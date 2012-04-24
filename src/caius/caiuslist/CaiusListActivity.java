f216694ef9ce90d55c469885b06d32e999838a0b
lvicainne <louis.vicainne@gmail.com>
2012-04-24 11:20:29 +0200

    CaiusList avec les fichiers initaux

---
+++ src/caius/caiuslist/CaiusListActivity.java
@@ -0,0 +1,13 @@
+package caius.caiuslist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CaiusListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
